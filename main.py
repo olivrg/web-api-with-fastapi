911d0bde0badfceca9cbfc4b8e9bf52509235d0b
Oli Guei <olivrg@gmail.com>
2023-04-26 10:09:16 +0100

    Use pydantic to declare json data models

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
+from typing import Optinal
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Optinal[str] = None
+    price: float
+    tax: Optinal[float] = None
+
 
 app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
 
 
 # @app.get("/items/{item_id}")
 # async def read_item(item_id: int):
 #     return {"item_id": item_id}
 
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
+# @app.get("/users/me")
+# async def read_user_me():
+#     return {"user_id": "the current user"}
 
 
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+# @app.get("/users/{user_id}")
+# async def read_user(user_id: str):
+#     return {"user_id": user_id}
